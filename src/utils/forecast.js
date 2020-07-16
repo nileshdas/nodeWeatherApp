0a65ccb0b0a62aabf154adb9267fb8e725b4ca9f
Nilesh Das <nileshdas50@gmail.com>
2020-07-16 21:26:27 +0530

    Initial Commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+  const url = 'https://api.openweathermap.org/data/2.5/onecall?lat='+ latitude + '&lon='+ longitude + '&exclude=hourly,daily&units=metric&appid=7ea4e689b3bfc4c9985b8e794b61922b'
+
+  request({url, json: true}, (error, {body}) => {
+      if(error){
+        callback('Unable to fetch weather', undefined)
+      }else if(body.message){
+        callback('Invalid Location', undefined)
+      }
+      else{
+        callback(undefined, 'It is currently ' + body.current.temp + ' degrees outside. And the general weather is ' + body.current.weather[0].description )
+      }
+  })
+}
+
+module.exports = forecast
---
+++ src/utils/geocode.js
@@ -0,0 +1,19 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+  const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibmlsZXNoZGFzNTAiLCJhIjoiY2tjN3ozaGo4MTZicDJ5bWdzNmlsYnBwcCJ9.WtfQECc-V0gI4-nvQ8PJ4Q'
+  request({ url:url, json: true}, (error, { body }) => {
+    if (error){
+      callback('Unable to connect to services', undefined)
+    }else if (body.features.length === 0){
+      callback('Unable to find location', undefined)
+    }else{
+      callback(undefined, {
+         latitude : body.features[0].center[1],
+         longitude : body.features[0].center[0],
+         location : body.features[0].place_name
+      })
+    }
+  })
+}
+module.exports = geocode
